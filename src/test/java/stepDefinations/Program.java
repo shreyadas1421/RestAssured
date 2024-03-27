f43942d1508651be3452add44f4ef50f73b600ea
Shreya <shreyadas1421@gmail.com>
2024-03-27 11:28:17 -0400

    Second commit_Shreya

---
+++ src/test/java/stepDefinations/Program.java
@@ -0,0 +1,32 @@
+package stepDefinations;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import Utilities.datareader;
+import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class Program{
+	
+	datareader d=new datareader();
+
+	@Given("userable to log in")
+	public void userable_to_log_in() throws IOException {
+		RestAssured.baseURI="https://lms-marchapi-hackathon-a258d2bbd43b.herokuapp.com/lms";
+		String response=given().log().all().header("Content-Type","application/json").
+				body(d.login()).when().post("/login")
+				.then().log().all().assertThat().statusCode(200).extract().response().asString();
+				
+				System.out.println(response);
+				
+		JsonPath js= new JsonPath(response);
+		String token_id=js.getString("token");
+		
+		System.out.println(token_id);
+		
+	}
+}
